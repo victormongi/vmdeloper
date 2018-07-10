65dc7925ce328cef577bdee6dd009068d6598130
Victor Mongi <victor.mongi@gmail.com>
2018-07-10 14:30:41 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,8 +91,7 @@
 
             <div class="container">
                 <div class="title">
-                        <img class="img-vmdeloper" src="{{ asset('images/vm.gif') }}" alt="vmdeloper logo">
-                <h1>Testtttttt</h1>
+                    <img class="img-vmdeloper" src="{{ asset('images/vm.gif') }}" alt="vmdeloper logo">
                 </div>
                 <p>Projects:</p>
                 <div class="links"><a href="http://dikbud.vmdeloper.com">DIKBUD App Dev</a></div>
